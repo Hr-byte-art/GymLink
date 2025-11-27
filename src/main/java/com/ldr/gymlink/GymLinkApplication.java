1886580680b06367409408934a64d565d7ce175f
wang-haha <wanghaha1217@126.com>
2025-11-27 22:39:41 +0800

    初始化项目

---
+++ src/main/java/com/ldr/gymlink/GymLinkApplication.java
@@ -0,0 +1,13 @@
+package com.ldr.gymlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ldr/gymlink/GymLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ldr.gymlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
